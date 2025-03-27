1e1595876612f6523f76b39cad4ce9658a0b1a8a
diogodevc <diogo1045@gmail.com>
2025-03-27 14:57:02 -0300

    Update supertrunfo.c
    adição de mais dados e a implementação de calculo de densidade e pib per capita das cidades

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,22 +1,49 @@
 #include <stdio.h>
 
 int main(){
+// informaões cadastrais das cidades brasileiras
+    char carta = 'B';
+         char carta2 = 'C';
+    char codigo[] = "B01";
+         char codigo2[] = "C01";
+    int populacao1 = 2418000;
+        int populacao2 = 2428678;
+    int pturisticos = 20;
+        int pturisticos2 = 18;
+    float area1 = 693.8;
+          float area2 = 313.8;
+    double pib1 = 62.954000;
+          double pib2 = 73.000300;
+    char cidade[] = "Salvador";
+          char cidade2[] = "Fortaleza";
+//agora e fazer um printf e scanf para que seja coletado as informação de qual cidade escolhera
 
-// informações das cidades.
-    char estado1 = 'B', estado2 = 'C';
-    char codigo1[] = "B01", codigo2[] = "C01";
-    char cidade1[] = "Salvador", cidade2[] = "Fortaleza";
-    int populaçao1 = 2418000, populaçao2 = 2428678;
-    float area1 = 693.8, area2 = 313.8;
-    float pib1 = 210.70, pib2 = 730.00;
-
-// usuario ira digitar as duas cidades e as informaçoes aparecem na tela
-    printf("Digite sua cidade: ");
-    scanf("%s %s", cidade1, cidade2);
-    printf("Estado: %c, %c\nCidade: %s, %s\nCódigo: %s, %s\n", estado1, estado2, cidade1, cidade2, codigo1, codigo2);
-    printf("Populção: %d, %d\nPib:%.2f, %.2f", populaçao1, populaçao2, area1, area2, pib1, pib2);
-
-
+printf("*** Escolha uma dentre duas opcoes abaixo ***\n");
+printf("Escolha A ou B\n");
+scanf("%c");
+      printf("Sua escolha e: %c\n", carta);
+      printf("Sua cidade e: %s\n", cidade);
+      printf("Codigo: %s\n", codigo);
+      printf("Pontos turisticos da cidade: %d\n", pturisticos);
+      printf("Area total: %.1f\n", area1);
+      printf("Pib da cidade: %f\n", pib1);
+// Usando operacao de divisão para calcular a densidade da cidade na variavel float
+float divisao = populacao1 / area1;
+// Usando operacao de divisão para calcular a pib da cidade na variavel doable
+double divisao2 = populacao1 / pib1;
+printf("a densidade de Salvador e: %.2f\n", divisao);
+printf("O pib per capita de Salvador e: %.2f\n", divisao2);
+// como no momento o programa so repete as informações ainda nao a sistema de escolha eu coloquei as infomações da outra cidade abaixo
+float divisao3 = populacao2 / area2;
+double divisao4 = populacao2 / pib2;
+printf("*** Informações da outra carta: %c ***\n", carta2);
+printf("cidade: %s\n", cidade2);
+       printf("codigo: %s\n", codigo2);
+       printf("a densidade de Fortaleza e: %.2f\n", divisao3);
+       printf("O pib per capita de Fotaleza e: %.2f\n", divisao4);
+       printf("Pontos turisticos da cidade: %d\n", pturisticos2);
+       printf("Area total: %.1f\n", area2);
+       printf("Pib da cidade: %f\n", pib2);
 
 
 
